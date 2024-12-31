57796928a26fb89ddc1a385779be5ac0ace56204
JackPro-12 <jackpro12@baworzz.my.id>
2024-12-31 14:50:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7887587003:AAHVO_TQZoehri88ib43xbC373jyGDELfRU", // Token bot Telegram
+  OWNER_ID: "7775351919", // ID pemilik bot
+  allowedGroupIds: [-1002374857091], // ID grup yang diizinkan
+};
